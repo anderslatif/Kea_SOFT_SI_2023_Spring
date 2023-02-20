16a35d80b0a250c7c114180291524139e51e8376
Anders Latif <andl@kea.dk>
2023-02-20 11:52:32 +0100

    Started using ngrok

--- 06._server_to_server/node/app.js
+++ 06._server_to_server/node/app.js
@@ -7,8 +7,11 @@ app.get("/date", (req, res) => {
 
 app.get("/datefromfastapi", async (req, res) => {
 /* task get the date from fastapi. Complete the following: */
-    fetch("http://127.0.0.1:8000/date")
+    const response = await fetch("http://127.0.0.1:8000/date");
+    const date = await response.json();
+    res.send(date);
 });
 
 const PORT = 8080;
 app.listen(PORT, () => console.log("Server is running on port", PORT));
+
